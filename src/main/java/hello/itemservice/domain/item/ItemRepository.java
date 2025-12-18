d06025af3a7ebca0fe0b3bcefd90ffc5d1e1590b
hyos <hyozzang99@gmail.com>
2025-12-18 23:36:38 +0900

    Feat: 상품 수정 및 결과 컨트롤러 및 뷰 생성

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -14,19 +14,20 @@ public class ItemRepository {
     public Item save(Item item) {
         item.setId(++sequence);
         store.put(item.getId(), item);
+
         return item;
     }
 
-    public Item findById(Long id) {
-        return store.get(id);
+    public Item findById(Long itemId) {
+        return store.get(itemId);
     }
-    
+
     public List<Item> findAll() {
         return new ArrayList<>(store.values());
     }
 
     public void update(Long itemId, Item updateParam) {
-        Item findItem = findById(itemId);
+        Item findItem = store.get(itemId);
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
@@ -35,4 +36,29 @@ public class ItemRepository {
     public void clearStore() {
         store.clear();
     }
+
+//    public Item save(Item item) {
+//        item.setId(++sequence);
+//        store.put(item.getId(), item);
+//        return item;
+//    }
+//
+//    public Item findById(Long id) {
+//        return store.get(id);
+//    }
+//
+//    public List<Item> findAll() {
+//        return new ArrayList<>(store.values());
+//    }
+//
+//    public void update(Long itemId, Item updateParam) {
+//        Item findItem = findById(itemId);
+//        findItem.setItemName(updateParam.getItemName());
+//        findItem.setPrice(updateParam.getPrice());
+//        findItem.setQuantity(updateParam.getQuantity());
+//    }
+//
+//    public void clearStore() {
+//        store.clear();
+//    }
 }
--- src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -21,39 +21,42 @@ class ItemRepositoryTest {
     void save() {
         //given
         Item item = new Item("itemA", 10000, 10);
+
         //when
         Item savedItem = itemRepository.save(item);
+
         //then
         Item findItem = itemRepository.findById(item.getId());
         assertThat(findItem).isEqualTo(savedItem);
-        assertThat(item.getItemName()).isEqualTo(savedItem.getItemName());
     }
-
+    
     @Test
-    void findAll() {
+    void findAll() throws Exception {
         //given
         Item item1 = new Item("itemA", 10000, 10);
-        Item item2 = new Item("itemA", 10000, 20);
+        Item item2 = new Item("itemB", 20000, 20);
+
         itemRepository.save(item1);
         itemRepository.save(item2);
 
         //when
-        List<Item> result = itemRepository.findAll();
+        List<Item> items = itemRepository.findAll();
 
         //then
-        assertThat(result.size()).isEqualTo(2);
-        assertThat(result).contains(item1, item2);
+        assertThat(items.size()).isEqualTo(2);
+        assertThat(items).containsExactly(item1, item2);
     }
-
+    
     @Test
-    void updateItem() {
+    void update() throws Exception {
         //given
         Item item = new Item("itemA", 10000, 10);
+
         Item savedItem = itemRepository.save(item);
         Long itemId = savedItem.getId();
 
         //when
-        Item updateParam = new Item("updateItem", 20000, 20);
+        Item updateParam = new Item("item2", 500, 1);
         itemRepository.update(itemId, updateParam);
 
         //then
